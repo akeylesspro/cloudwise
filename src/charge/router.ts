ed1d15bef0bd96280c692c9667b3e5a3da3ee126
avrhahm sason <avi6190i@gmail.com>
2026-02-09 17:13:27 +0200

    finish

--- src/charge/router.ts
+++ src/charge/router.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { get_cdrs, get_location_status, get_locations, stop_session_service, simulate_session_service } from "./services";
+import { get_cdrs, get_location_status, get_locations, stop_session_service, simulate_session_service, start_session_service } from "./services";
 import { mandatory, nx_user_login, verify_user_auth } from "akeyless-server-commons/middlewares";
 import package_json from "../../package.json";
 
@@ -18,6 +18,22 @@ router.get("/locations/status", get_location_status);
 router.post("/locations/get", get_locations);
 
 router.post("/sessions/stop", verify_user_auth, mandatory({ body: [{ key: "car_number", type: "string", length: 3 }] }), stop_session_service);
+
+router.post(
+    "/sessions/start",
+    verify_user_auth,
+    mandatory({
+        body: [
+            { key: "car_number", type: "string", length: 3 },
+            { key: "location_id", type: "string" },
+            { key: "station_uid", type: "string" },
+            { key: "connector_id", type: "string" },
+            { key: "lat", type: "number" },
+            { key: "lng", type: "number" },
+        ],
+    }),
+    start_session_service
+);
 
 router.post(
     "/sessions/simulate",
